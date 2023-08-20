617461d4724b907c72e652621b8cbf1e42445d16
Aparna2726 <73382952+Aparna2726@users.noreply.github.com>
2023-08-21 01:22:14 +0530

    Time: 0 ms (100.00%), Space: 50.3 MB (93.15%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        
+        while (left < right) {
+            // Swap characters without using a temporary variable
+            s[left] ^= s[right];
+            s[right] ^= s[left];
+            s[left] ^= s[right];
+            
+            left++;
+            right--;
+        }
+    }
+}
